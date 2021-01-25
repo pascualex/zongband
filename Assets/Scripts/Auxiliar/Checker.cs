a71d7e479de38fb6afd2ec2db321b8933a2caaca
Alejandro Pascual Pozo <alejandro.pascual.pozo@gmail.com>
2021-01-25 13:58:09 +0100

    [refactor] static goes after visibility

--- Assets/Scripts/Auxiliar/Checker.cs
+++ Assets/Scripts/Auxiliar/Checker.cs
@@ -6,19 +6,19 @@ public class Checker {
     
     private Checker() { }
 
-    static public bool Range(float value, float max) {
+    public static bool Range(float value, float max) {
         return Range(value, 0, max);
     }
 
-    static public bool Range(float value, float min, float max) {
+    public static bool Range(float value, float min, float max) {
         return value >= 0 && value < max;
     }
 
-    static public bool Range(Vector2 value, Vector2 max) {
+    public static bool Range(Vector2 value, Vector2 max) {
         return Range(value, Vector2.zero, max);
     }
 
-    static public bool Range(Vector2 value, Vector2 min, Vector2 max) {
+    public static bool Range(Vector2 value, Vector2 min, Vector2 max) {
         return Range(value.x, min.x, max.x) && Range(value.y, min.y, max.y);
     }
 }
